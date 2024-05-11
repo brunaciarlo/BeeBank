e53b61226d81b286cda73d0ea6198572e0cfc18f
bruna <bruciarlo@yahoo.com.br>
2024-05-10 21:58:37 -0300

    adição db

---
+++ db/bee_bank.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 11-Maio-2024 às 02:53
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bee_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `chaves_pix`
+--
+
+CREATE TABLE `chaves_pix` (
+  `id` int(11) NOT NULL,
+  `tipo_chave` varchar(10) DEFAULT NULL,
+  `chave_pix` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `extrato`
+--
+
+CREATE TABLE `extrato` (
+  `id` int(11) NOT NULL,
+  `tipo_transferencia` varchar(10) DEFAULT NULL,
+  `valor_transferencia` decimal(10,2) DEFAULT NULL,
+  `saldo` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `extrato`
+--
+
+INSERT INTO `extrato` (`id`, `tipo_transferencia`, `valor_transferencia`, `saldo`) VALUES
+(1, 'Deposito', '3600.00', '3600.00'),
+(2, 'Saque', '250.00', '3350.00'),
+(3, 'Pix', '30.00', '3320.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(150) DEFAULT NULL,
+  `chave_pix` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `favoritos`
+--
+
+INSERT INTO `favoritos` (`id`, `nome`, `chave_pix`) VALUES
+(1, 'Maria Julia', '21 998877665'),
+(2, 'Pedro Antônio', '12378945600');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `chaves_pix`
+--
+ALTER TABLE `chaves_pix`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `extrato`
+--
+ALTER TABLE `extrato`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `chaves_pix`
+--
+ALTER TABLE `chaves_pix`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `extrato`
+--
+ALTER TABLE `extrato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de tabela `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
